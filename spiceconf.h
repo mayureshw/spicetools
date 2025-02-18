0bf83b5e10bbbf11b5aac67c8cc423d202420b6c
guest@nuc <guest@nuc>
2025-02-19 04:28:55 +0530

    logic threshold made more precise

--- spiceconf.h
+++ spiceconf.h
@@ -8,6 +8,6 @@ using namespace std;
 const string rawopfile = "simuop.raw";
 const double vdd = 1.8;
 const string vddstr = to_string(vdd);
-const double logicthresh = vdd / 2;
+const double logicthresh = 0.81; // Can be .45 to .55 of Vdd
 
 #endif
